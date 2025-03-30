1cc92e76d1896234458c4f4bf24f6abb0268fbbe
dnlipko <dmitrii.lipko@gmail.com>
2025-03-30 19:23:30 +0200

    chore: update GitHub workflow and Next.js config for better GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: '',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   trailingSlash: true,
   reactStrictMode: true,
 }
